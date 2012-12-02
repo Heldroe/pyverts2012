b4d8cb71aae9f5cc43451324a2c4372fa2072927
David Guerrero <heldroe@gmail.com>
2012-12-02 19:45:14 +0100

    first 360 views

---
+++ team360/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'team360.views.home', name='team360_home'),
+)
--- team360/views.py
+++ team360/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from datetime import datetime, timedelta
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from team360.models import WeekRater
+
+@login_required
+def home (request):
+    ratedlist = WeekRater.objects.filter(rater=request.user, date__gt=datetime.now()-timedelta(weeks=1))
+    return render(request, 'team360.html', {'ratedlist': ratedlist})
